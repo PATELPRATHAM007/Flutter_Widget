60f8ea8e2d4d1b767b274e510c86dee03a353f47
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-01-21 13:26:06 +0530

    text widget properties

--- lib/01.1_Text_Properties.dart
+++ lib/01.1_Text_Properties.dart
@@ -14,12 +14,12 @@ class _TextPropertiesState extends State<TextProperties> {
       body: Align(
         alignment: Alignment(-1, -0.8),
         child: Text(
-              "my name is pratham patel fhfisahfhafofsfdfsfatf7astffhifg8aohigaukgy", // string which using for disply textin screen
+              "my name is pratham patel fhfisahfhafofsfdfsfatf7astffhifg8aohigaukgy", // string which using for disply textin screen and also this string is positional parameter
               // textDirection: TextDirection.rtl, // use for deside the direction for text means text start at left side to right side
               textAlign: TextAlign.justify, // use for set text alignment left, right, right and also justify 
               // overflow: TextOverflow.ellipsis, // if text is overflow means use  when text is not fit up in to display
               softWrap: true, // etermines whether the text should be allowed to wrap to the next line when it exceeds the available width of the container.
-              //  maxLines: 2, // show or Write up to 2 lines only 
+              maxLines: 2, // show or Write up to 2 lines only 
               semanticsLabel: "faltu", // descrption 
               style: TextStyle( // text style use for adding style to the text widget like fontsize,fony weight,color and many other
                 fontSize: 30
