9531d151605d29edb3bddc11b59a0a9acf3e8f3a
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-01-29 23:16:35 +0530

    appbar

--- lib/16_appbar.dart
+++ lib/16_appbar.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 
-class AppBarWidget extends StatefulWidget {
-  const AppBarWidget({super.key});
+class AppBarExample extends StatelessWidget {
+  const AppBarExample({super.key});
 
-  @override
-  State<AppBarWidget> createState() => _AppBarWidgetState();
-}
-
-class _AppBarWidgetState extends State<AppBarWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[800],
+          leading: const Icon(Icons.access_alarm),
+          leadingWidth: 30,
+
+          title: const Text("Appbar example"),
+          titleTextStyle: const TextStyle(color: Colors.white, fontSize: 22),
+          // title: Container(
+          //   width: 50,
+          //   height: 50,
+          //   child: Image.network("url"),
+          // ),
+          centerTitle: true,
+          iconTheme: IconThemeData(color: Colors.white),
+          actions: const [Icon(Icons.more_vert)],
+          actionsIconTheme: const IconThemeData(color: Colors.white, size: 30),
+          elevation: 21,
+          shadowColor: Colors.pink,
+          toolbarHeight: 100,
+          toolbarOpacity: .5,
+        ),
+        // drawer: const Drawer(),
+      ),
+    );
   }
 }
