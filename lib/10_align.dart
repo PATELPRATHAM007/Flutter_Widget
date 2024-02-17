a0a8ffd82041e3af0432637ddc46eb41e23fea92
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-02-17 12:18:07 +0530

    align widget

--- lib/10_align.dart
+++ lib/10_align.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+
 class ALignWidget extends StatefulWidget {
   const ALignWidget({super.key});
 
@@ -11,10 +12,13 @@ class _ALignWidgetState extends State<ALignWidget> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        // align widget thaat aligns its child within itslef and optionally sizes itself based on the child's size.
         body: Align(
-          alignment: const Alignment(0, 0),
-          // heightFactor: 2,
-          // widthFactor: 2,
+          alignment: const Alignment(0.1,
+              0.4), // this property are use for align element in proper position
+          // heightFactor: 2, // similar to the column heightfactor
+          // widthFactor: 2,//  similar to the column heightfactor
+
           child: Container(
             width: 200,
             height: 200,
@@ -26,7 +30,6 @@ class _ALignWidgetState extends State<ALignWidget> {
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 22,
-              
                 ),
               ),
             ),
