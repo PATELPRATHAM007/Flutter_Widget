38430165efd0265a5e189796f9307cd667aa0466
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-02-17 12:21:36 +0530

    appbar widget

---
+++ lib/16.1_immediately_hide_appbar.dart
@@ -0,0 +1,62 @@
+// import 'package:flutter/material.dart';
+
+// class ImmediatelyHideAppbar extends StatelessWidget {
+//   const ImmediatelyHideAppbar({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: NestedScrollView(
+//           headerSliverBuilder: headerSliverBuilder,
+//            body: body
+//           ),
+//           // body: SingleChildScrollView(
+//           //     child: Column(
+//           //   children: [
+//           //     Container(
+//           //       height: 150,
+//           //       color: Colors.pink,
+//           //     ),
+//           //     Container(
+//           //       height: 150,
+//           //       color: Colors.yellow,
+//           //     ),
+//           //     Container(
+//           //       height: 150,
+//           //       color: Colors.black,
+//           //     ),
+//           //     Container(
+//           //       height: 150,
+//           //       color: Colors.orange,
+//           //     ),
+//           //     Container(
+//           //       height: 150,
+//           //       color: Colors.purple,
+//           //     ),
+//           //     Container(
+//           //       height: 150,
+//           //       color: Colors.pink,
+//           //     ),
+//           //     Container(
+//           //       height: 150,
+//           //       color: Colors.yellow,
+//           //     ),
+//           //     Container(
+//           //       height: 150,
+//           //       color: Colors.black,
+//           //     ),
+//           //     Container(
+//           //       height: 150,
+//           //       color: Colors.orange,
+//           //     ),
+//           //     Container(
+//           //       height: 150,
+//           //       color: Colors.purple,
+//           //     ),
+//           //   ],
+//           // )),
+//           ),
+//     );
+//   }
+// }
--- lib/16_appbar.dart
+++ lib/16_appbar.dart
@@ -7,8 +7,16 @@ class AppBarExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        extendBodyBehindAppBar: true,
         appBar: AppBar(
           backgroundColor: Colors.blue[800],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20))),
+          ),
+
           leading: const Icon(Icons
               .access_alarm), // leading property are mostly use for puting icon or icon buttons  or in a some case we can be use this part for puting our logo and name for company
           leadingWidth: 30, // use for putign some space to the starting part
@@ -20,18 +28,21 @@ class AppBarExample extends StatelessWidget {
           titleTextStyle: const TextStyle(
               color: Colors.white,
               fontSize: 22), // use for decoraction of title
+
           // title: Container(
           //   width: 50,
           //   height: 50,
           //   child: Image.network("url"),
           // ), // we are able to image on the appbar
           centerTitle: true, // set title in the center of appbar
+
           actions: const [
             Icon(Icons.more_vert)
           ], // use for set some action icons like setting icons.we can be set one or more action button
           actionsIconTheme: const IconThemeData(
               color: Colors.white, size: 30), // decoration for actions icons
-          elevation: 21, //eleveing of appbar
+
+          elevation: 0.0, //eleveing of appbar
           shadowColor: Colors.pink,
           toolbarHeight: 100, //set size of appbar
           // toolbarOpacity: .5, // ser opacity of elements ehich are available in appbar
