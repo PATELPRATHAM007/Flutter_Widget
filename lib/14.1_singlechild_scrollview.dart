0e122657c350aa5db88503b077a0bcc0ec66cff3
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-02-17 12:21:12 +0530

    scrollview widget

---
+++ lib/14.1_singlechild_scrollview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScrollControllerExample extends StatelessWidget {
+  const ScrollControllerExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
--- lib/14_singlechild_scrollview.dart
+++ lib/14_singlechild_scrollview.dart
@@ -1,23 +1,33 @@
 import "package:flutter/material.dart";
+
 class SingleChildScrollViewExample extends StatefulWidget {
   const SingleChildScrollViewExample({super.key});
 
   @override
-  State<SingleChildScrollViewExample> createState() => _SingleChildScrollViewExampleState();
+  State<SingleChildScrollViewExample> createState() =>
+      _SingleChildScrollViewExampleState();
 }
 
-class _SingleChildScrollViewExampleState extends State<SingleChildScrollViewExample> {
+class _SingleChildScrollViewExampleState
+    extends State<SingleChildScrollViewExample> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SingleChildScrollView(
-          // padding: EdgeInsets.all(20),
-          // physics: ScrollPhysics(),
-          // physics: NeverScrollableScrollPhysics(),
-          // physics: FixedExtentScrollPhysics(),
+           
+          //  keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.manual,
+           keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+
+          // padding: EdgeInsets.all(20), // give padding
+          // physics: ScrollPhysics(), // similar to the scroll
+          // physics: NeverScrollableScrollPhysics(), //remove scroll
+          // physics: FixedExtentScrollPhysics(), // we scroll scrren using only mouse scroller
+          // physics: BouncingScrollPhysics(), // we scroll scrren using only mouse scroller
+
           // reverse: true,
-          scrollDirection: Axis.vertical,
+          scrollDirection: Axis
+              .vertical, //set scroll direction either vertical or horizontal
           child: Column(
             children: [
               Container(
