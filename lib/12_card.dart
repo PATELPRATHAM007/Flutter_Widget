cbd9774219737bd3fcb0089db99da2e7e8025cff
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-02-17 12:18:49 +0530

    card widget

--- lib/12_card.dart
+++ lib/12_card.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+
 class CardWidget extends StatefulWidget {
   const CardWidget({super.key});
 
@@ -14,16 +15,40 @@ class _CardWidgetState extends State<CardWidget> {
         body: Container(
           width: 200,
           height: 200,
-          margin: EdgeInsets.all(5),
-          child: Card(
-            color: Colors.blue,
-            // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular((20))), // cardcorner control
-            elevation: 10,// shadow control
-            shadowColor: Colors.grey,
-            surfaceTintColor: Colors.green,
+          // margin: const EdgeInsets.all(5),
+
+          // material design card :a panel with dlightly rounded corner s and an elevation shadow
+          child: const Padding(
+            padding: EdgeInsets.all(16),
+            child: Card(
+              borderOnForeground:
+                  true, //using this proerty we set border of card widget in front of child or behind of child, if value ids trure than border are in front of child eles behind the child
+
+              clipBehavior: Clip
+                  .hardEdge, // the border are cuted  if chid are bigger than card.its using for set image inside of card widget
+
+              margin: EdgeInsets.all(20), // for margin
+              color: Colors.blue, // set card color
+              // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular((20))), // cardcorner control
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.only(bottomRight: Radius.circular(20)),
+                side: BorderSide(
+                    color: Colors.red, style: BorderStyle.solid, width: 300),
+              ), // cardcorner control
+              elevation: 10, // shadow control
+              shadowColor: Colors.grey, //set  shadow colors
+              surfaceTintColor: Colors.green,
+            ),
           ),
         ),
       ),
     );
   }
 }
+// some limitation of card widget
+/*
+1. card widget are not contain padding property
+2. also we can't not give the spacific size witout using other widget(we can give size of card by using container or sizedbox)
+
+*/
